aa82e565ccef79005093cfa2c0df60dd7f098196
LAPTOP-bqkong\kong <ordinary_kong077@163.com>
2023-12-15 00:39:16 +0800

    增加script

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -5,21 +5,54 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [Serializable]
-public class MClick : UnityEvent<Vector3> { }
+public class EventVector3 : UnityEvent<Vector3> { }
 
 public class MouseManager : MonoBehaviour
 {
+    public EventVector3 OnMouseClicked;
+
+    RaycastHit hitInfo;
+
+    //和上面的写法效果一样
+    //public UnityEvent<Vector3> OnMouse;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        SetCursorTexture();
+        MouseControl();
 
     }
 
+    void SetCursorTexture()
+    { 
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var state = Physics.Raycast(ray, out hitInfo);
+        if(state)
+        {
+           
+        }
+
+    }
+
+    void MouseControl()
+    {
+        if (Input.GetMouseButtonDown(0) && hitInfo.collider != null)
+        { 
+            if(hitInfo.collider.gameObject.CompareTag("Ground"))
+            {
+                OnMouseClicked?.Invoke(hitInfo.point);
+            }
+
+        }
+
+
+    }
 
 }
