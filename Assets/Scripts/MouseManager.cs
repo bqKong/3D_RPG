da3ae699083e4480cce92052577220d64b5ef7ab
LAPTOP-bqkong\kong <ordinary_kong077@163.com>
2023-12-14 23:36:06 +0800

    3DRPG

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class MClick : UnityEvent<Vector3> { }
+
+public class MouseManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
