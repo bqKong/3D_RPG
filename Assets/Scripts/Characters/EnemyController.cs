fb1af785b718792658d61df73e8f860b5b78faac
kbq <1245928277@qq.com>
2023-12-20 18:01:43 +0800

    12/20

--- Assets/Scripts/Characters/EnemyController.cs
+++ Assets/Scripts/Characters/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -15,28 +16,88 @@ public enum EnemyStates
 [RequireComponent(typeof(NavMeshAgent))]
 public class EnemyController : MonoBehaviour
 {
-    public EnemyStates enemyStates;
+    [Header("Basic Settings")]
+    public float sightRadius;
+    public bool isGuard;
+
+    //左顾右盼时间
+    public float lookAtTime;
+
+    [Header("Patrol State")]
+    public float patrolRange;
+
+    private EnemyStates enemyStates;
     private NavMeshAgent agent;
+    private Animator anim;
+
+    private GameObject attackTarget;
+
+
+    //记录原来速度
+    private float speed;
+
+    private Vector3 wayPoint;
+
+    //初始坐标
+    private Vector3 guardPos;
+
+    private float remainLookAtTime;
+
+    //bool 配合动画
+    bool isWalk;
+    bool isChase;
+    bool isFollow;
+
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+        speed = agent.speed;
+        guardPos = transform.position;
+        remainLookAtTime = lookAtTime;
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        if (isGuard)
+        {
+            enemyStates = EnemyStates.GUARD;
+        }
+        else
+        {
+            enemyStates = EnemyStates.PATROL;
+            GetNewWayPoint();
+        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        SwitchStates();
+        SwitchAnimation();
+
+    }
+
+    private void SwitchAnimation()
+    {
+        anim.SetBool("Walk", isWalk);
+        anim.SetBool("isChase", isChase);
+        anim.SetBool("Follow", isFollow);
 
     }
 
     private void SwitchStates()
     {
+        //发现player 切换到CHASE
+        if (FoundPlayer())
+        {
+            enemyStates = EnemyStates.CHASE;
+        }
+
         switch (enemyStates)
         {
             case EnemyStates.GUARD:
@@ -45,9 +106,65 @@ public class EnemyController : MonoBehaviour
 
             case EnemyStates.PATROL:
 
+                isChase = false;
+                agent.speed = speed * 0.5f;
+
+                //判断是否到了随机巡逻队
+                if (Vector3.Distance(wayPoint, transform.position) <= agent.stoppingDistance)
+                {
+                    isWalk = false;
+                    if (remainLookAtTime > 0)
+                    {
+                        remainLookAtTime -= Time.deltaTime;
+                    }
+                    else
+                    {
+                        GetNewWayPoint();
+                    }
+
+                }
+                else
+                {
+                    isWalk = true;
+                    agent.destination = wayPoint;
+                }
+
                 break;
 
             case EnemyStates.CHASE:
+
+                isWalk = false;
+                isChase = true;
+                agent.speed = speed;
+                //TODO:追player
+                if (!FoundPlayer())
+                {
+                    //拉脱回到上一个状态
+                    isFollow = false;
+                    if (remainLookAtTime > 0)
+                    {
+                        //保持原位不动
+                        agent.destination = transform.position;
+                        remainLookAtTime -= Time.deltaTime;
+                    }
+                    else if (isGuard)
+                    {
+                        enemyStates = EnemyStates.GUARD;
+                    }
+                    else
+                    {
+                        enemyStates = EnemyStates.PATROL;
+                    }
+                }
+
+                //追击
+                else
+                {
+                    isFollow = true;
+                    agent.destination = attackTarget.transform.position;
+                }
+
+                //TODO:在攻击范围内攻击，配合动画
 
                 break;
 
@@ -56,5 +173,61 @@ public class EnemyController : MonoBehaviour
                 break;
         }
     }
+
+    bool FoundPlayer()
+    {
+        var colliders = Physics.OverlapSphere(transform.position, sightRadius);
+        foreach (var collider in colliders)
+        {
+            if (collider.CompareTag("Player"))
+            {
+                attackTarget = collider.gameObject;
+                return true;
+            }
+
+        }
+
+        attackTarget = null;
+        return false;
+    }
+
+    bool TargetInAttackRange()
+    { 
+    
+    }
+
+    bool TargetInSkillRange()
+    { 
+    
+        
+    }
+
+
+
+    void GetNewWayPoint()
+    {
+        remainLookAtTime = lookAtTime;
+
+        //随机生成新的点
+        float randomX = Random.Range(-patrolRange, patrolRange);
+        float randomZ = Random.Range(-patrolRange, patrolRange);
+
+        Vector3 randomPoint = new Vector3(guardPos.x + randomX, transform.position.y, guardPos.z + randomZ);
+
+        //FIXME:可能有问题
+        //wayPoint = randomPoint;
+
+        //修复问题
+        NavMeshHit hit;
+        wayPoint = NavMesh.SamplePosition(randomPoint, out hit, patrolRange, 1) ? hit.position : transform.position;
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, sightRadius);
+    }
+
 
 }
--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,23 +9,34 @@ public class PlayerController : MonoBehaviour
 
     private NavMeshAgent agent;
     private Animator anim;
+    private CharacterStats characterStats;
+
+    private GameObject attackTarget;
+
+    [Tooltip("冷却时间")]
+    private float lastAttackTime;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
+        characterStats = GetComponent<CharacterStats>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
         MouseManager.Instance.OnMouseClicked += MoveToTarget;
+        MouseManager.Instance.OnEnemyClicked += EventAttack;
     }
+
 
     // Update is called once per frame
     void Update()
     {
         SwitchAnimation();
+
+        lastAttackTime -= Time.deltaTime;
     }
 
     private void SwitchAnimation()
@@ -38,7 +50,49 @@ public class PlayerController : MonoBehaviour
     /// <param name="target"></param>
     public void MoveToTarget(Vector3 target)
     { 
+        StopAllCoroutines();
+        agent.isStopped = false;
         agent.destination = target;
+    }
+
+    public void EventAttack(GameObject target)
+    {
+        if (target != null)
+        {
+            attackTarget = target;
+
+            //TODO:移动到面前并攻击
+            //不停移动直到站到怪物跟前，不用能while，要用协程
+            StartCoroutine(MoveToAttackTarget());
+
+        }
+
+    }
+
+    IEnumerator MoveToAttackTarget()
+    {
+        agent.isStopped = false;
+        transform.LookAt(attackTarget.transform);
+
+        //TODO:修改范围参数
+        while (Vector3.Distance(attackTarget.transform.position, this.transform.position) > characterStats.attackData.attackRange)
+        { 
+            agent.destination = attackTarget.transform.position;
+            yield return null;
+        }
+
+        //停下来
+        agent.isStopped = true;
+
+        //Attack
+        if (lastAttackTime < 0)
+        {
+            anim.SetTrigger("Attack");
+
+            //重置冷却时间
+            lastAttackTime = 0.5f;
+        }
+
     }
 
 }
